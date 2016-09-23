576b769578ea4758b41f89adcc32f0e01b10b720
Pattnatt <jittapat007@gmail.com>
2016-09-23 11:15:54 +0700

    Initailize Project

---
+++ desktop/src/com/oop/pacman/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.oop.pacman.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.oop.pacman.game.PacmanGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 600;
+		new LwjglApplication(new PacmanGame(), config);
+	}
+}
