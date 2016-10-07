a93ff648befc0f6368edffcd1aa46a2b8185c4be
Pattnatt <jittapat007@gmail.com>
2016-10-07 10:01:23 +0700

    Create GameScreen Class

---
+++ core/src/com/oop/pacman/game/GameScreen.java
@@ -0,0 +1,18 @@
+package com.oop.pacman.game;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+public class GameScreen extends ScreenAdapter{
+	private PacmanGame pacmanGame;
+	
+	public GameScreen(PacmanGame pacmanGame)
+	{
+		this.pacmanGame = pacmanGame;
+	}
+	
+	@Override
+	public void render(float delta)
+	{
+		System.out.println("Hello " + delta);
+	}
+}
